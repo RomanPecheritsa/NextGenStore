315173e1c18c5493d1dc4afc79877c6e4213f491
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-29 22:33:40 +0300

    add app catalog + html

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get('message')
+
+        print(f'{name} ({email}): {message}')
+    return render(request, 'catalog/contact.html')
