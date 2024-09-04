8f5984945e138c386c186b03b3b2cc6352c795c1
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-04 17:08:49 +0300

    add ContactInfoAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Category, Product
+from catalog.models import Category, Product, ContactInfo
 
 
 @admin.register(Product)
@@ -12,3 +12,8 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
+
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    list_display = ("phone", "email", "address")
