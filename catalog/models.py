e5837a59e58161ab480280c9d22c53ad0fe2f3a6
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-04 15:43:57 +0300

    linter module

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from decimal import Decimal
 
 
-BLANK_NULL_TRUE = {'blank': True, 'null': True}
+BLANK_NULL_TRUE = {"blank": True, "null": True}
 
 
 def upload_to(instance, filename):
@@ -19,41 +19,52 @@ def upload_to(instance, filename):
     Example:
         If the model is `Product` and the file name is `image.jpg`, the path will be `uploads/product/image.jpg`.
     """
-    return f'uploads/{instance.__class__.__name__.lower()}/{filename}'
+    return f"uploads/{instance.__class__.__name__.lower()}/{filename}"
 
 
 class Category(models.Model):
     """
     A model for storing product categories.
     """
-    name = models.CharField(max_length=50, verbose_name='Наименование категории')
-    description = models.TextField(verbose_name='Описание категории')
+
+    name = models.CharField(max_length=50, verbose_name="Наименование категории")
+    description = models.TextField(verbose_name="Описание категории")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name']
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
 
 
 class Product(models.Model):
     """
     A model for storing information about products.
     """
-    name = models.CharField(max_length=50, verbose_name='Наименование товара')
-    description = models.TextField(**BLANK_NULL_TRUE, verbose_name='Описание товара')
-    preview = models.ImageField(**BLANK_NULL_TRUE, upload_to=upload_to, verbose_name='Изображение товара')
+
+    name = models.CharField(max_length=50, verbose_name="Наименование товара")
+    description = models.TextField(**BLANK_NULL_TRUE, verbose_name="Описание товара")
+    preview = models.ImageField(
+        **BLANK_NULL_TRUE, upload_to=upload_to, verbose_name="Изображение товара"
+    )
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'), verbose_name='Цена за покупку')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal("0.00"),
+        verbose_name="Цена за покупку",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-        ordering = ['name', '-price', 'created_at', '-updated_at']
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name", "-price", "created_at", "-updated_at"]
