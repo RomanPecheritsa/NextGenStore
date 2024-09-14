48254c48e8a85fd9976254db7aaa5b1c85943039
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-14 11:26:40 +0300

    changed path

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name="home"),
     path("contact/", contact, name="contact"),
-    path("products/<int:pk>", product, name="product"),
+    path("products/<int:pk>/", product, name="product"),
 ]
