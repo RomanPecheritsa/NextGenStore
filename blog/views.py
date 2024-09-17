34e1f42f1a098c7394d1aa66ea08a6691722498a
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-18 01:44:51 +0300

    add ListView, DetailView Article

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Article
+
+
+class ArticlesListView(ListView):
+    model = Article
+    paginate_by = 10
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
