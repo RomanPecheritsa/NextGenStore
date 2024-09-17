23ff3dc50fc233bc5a538258c915199fb7802b4a
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-18 01:44:15 +0300

    add model Article

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from catalog.models import upload_to, BLANK_NULL_TRUE
+
+
+class Article(models.Model):
+    """
+    Blog post model
+    """
+    title = models.CharField(max_length=100, verbose_name="заголовок")
+    slug = models.SlugField(unique=True, verbose_name="slug", **BLANK_NULL_TRUE)
+    content = models.TextField(verbose_name="cодержимое статьи")
+    preview = models.ImageField(upload_to=upload_to, verbose_name="превью", **BLANK_NULL_TRUE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
+    is_published = models.BooleanField(default=True, verbose_name="опубликовано")
+    views_count = models.IntegerField(default=0, verbose_name="просмотры")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
+        ordering = ('-created_at',)
+
+
+
