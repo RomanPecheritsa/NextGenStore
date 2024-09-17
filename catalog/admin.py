4bf66862015748252956e5c6cd1086b277fb0fa8
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-18 01:42:09 +0300

    add admin blog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+
 from catalog.models import Category, Product, ContactInfo
+from blog.models import Article
 
 
 @admin.register(Product)
@@ -17,3 +19,10 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(ContactInfo)
 class ContactInfoAdmin(admin.ModelAdmin):
     list_display = ("phone", "email", "address")
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "created_at", "is_published", "slug")
+    list_filter = ("created_at", "is_published")
+    search_fields = ("title", "content")
