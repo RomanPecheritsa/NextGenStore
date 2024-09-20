569bb9ba77bba86b1795e233924a2a1c2327d5ae
pecheritsa <pecheritsa.roman@gmail.com>
2024-09-20 12:42:48 +0300

    changed load_fixtures add articles

--- catalog/management/commands/load_fixtures.py
+++ catalog/management/commands/load_fixtures.py
@@ -1,63 +1,47 @@
 import json
 from django.core.management.base import BaseCommand
 from catalog.models import Product, Category, ContactInfo
+from blog.models import Article
+
+PATH = "fixtures/load_data.json"
 
 
 class Command(BaseCommand):
     """
     Custom management command to populate the database with data from a JSON fixture file.
-    The command will first clear existing data from the Product, Category, and ContactInfo tables,
+    The command will first clear existing data from the Product, Category, ContactInfo, and Article tables,
     and then repopulate them with the data from the fixture file `catalog_data.json`.
     """
 
     @staticmethod
     def json_read_categories():
-        """
-        Reads and filters category data from the fixture file.
-
-        Returns:
-            list: A list of dictionaries containing data for Category model instances.
-        """
-        with open("fixtures/catalog_data.json", encoding="utf-8") as file:
+        with open(PATH, encoding="utf-8") as file:
             data = json.load(file)
             return [item for item in data if item["model"] == "catalog.category"]
 
     @staticmethod
     def json_read_products():
-        """
-        Reads and filters product data from the fixture file.
-
-        Returns:
-            list: A list of dictionaries containing data for Product model instances.
-        """
-        with open("fixtures/catalog_data.json", encoding="utf-8") as file:
+        with open(PATH, encoding="utf-8") as file:
             data = json.load(file)
             return [item for item in data if item["model"] == "catalog.product"]
 
     @staticmethod
     def json_read_contact_info():
-        """
-        Reads and filters contact information data from the fixture file.
-
-        Returns:
-            list: A list of dictionaries containing data for ContactInfo model instances.
-        """
-        with open("fixtures/catalog_data.json", encoding="utf-8") as file:
+        with open(PATH, encoding="utf-8") as file:
             data = json.load(file)
             return [item for item in data if item["model"] == "catalog.contactinfo"]
 
-    def handle(self, *args, **options):
-        """
-        Main method that handles the execution of the command.
+    @staticmethod
+    def json_read_articles():
+        with open(PATH, encoding="utf-8") as file:
+            data = json.load(file)
+            return [item for item in data if item["model"] == "blog.article"]
 
-        - Deletes all existing data from the Product, Category, and ContactInfo tables.
-        - Reads category, product, and contact information data from the fixture file.
-        - Creates new instances of Category, Product, and ContactInfo based on the fixture data.
-        - Saves the new instances in bulk to the database.
-        """
+    def handle(self, *args, **options):
         Product.objects.all().delete()
         Category.objects.all().delete()
         ContactInfo.objects.all().delete()
+        Article.objects.all().delete()
 
         categories_for_create = []
         for item in Command.json_read_categories():
@@ -79,5 +63,11 @@ class Command(BaseCommand):
             contact_data = item["fields"]
             contact_info_for_create.append(ContactInfo(id=item["pk"], **contact_data))
         ContactInfo.objects.bulk_create(contact_info_for_create)
+
+        articles_for_create = []
+        for item in Command.json_read_articles():
+            article_data = item["fields"]
+            articles_for_create.append(Article(id=item["pk"], **article_data))
+        Article.objects.bulk_create(articles_for_create)
 
         print("Success!")
