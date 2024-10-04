8e6e78aaf2e5e1a0d51e270626c4047822d9b266
pecheritsa <pecheritsa.roman@gmail.com>
2024-10-04 14:20:47 +0300

    changed product to CBV, functional formset

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,19 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from catalog.forms import ProductForm
-from catalog.models import Product, ContactInfo
+from django.forms import inlineformset_factory
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, ContactInfo, Version
 
 
 class MainTemplateView(TemplateView):
-    template_name = 'catalog/index.html'
+    template_name = "catalog/index.html"
 
 
 class ContactTemplateView(TemplateView):
@@ -14,7 +22,11 @@ class ContactTemplateView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["contact_info"] = ContactInfo.objects.first()
+        context["form"] = None
         return context
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
 
 
 class ProductListView(ListView):
@@ -30,18 +42,68 @@ class ProductDetailView(DetailView):
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["title"] = "Создание товара"
+        VersionFormset = inlineformset_factory(
+            Product, Version, form=VersionForm, extra=1
+        )
+        if self.request.method == "POST":
+            context_data["formset"] = VersionFormset(self.request.POST)
+        else:
+            context_data["formset"] = VersionFormset()
+        return context_data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context["formset"]
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
 
 
 class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy('catalog:product_list')
+    success_url = reverse_lazy("catalog:product_list")
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["title"] = "Редактирование товара"
+        VersionFormset = inlineformset_factory(
+            Product, Version, form=VersionForm, extra=1, can_delete=True
+        )
+        if self.request.method == "POST":
+            context_data["formset"] = VersionFormset(
+                self.request.POST, instance=self.object
+            )
+        else:
+            context_data["formset"] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context["formset"]
+        if form.is_valid() and formset.is_valid():
+            versions = [
+                form for form in formset if form.cleaned_data.get("is_active", False)
+            ]
+            if len(versions) > 1:
+                form.add_error(
+                    None, "У продукта не может быть более одной активной версии."
+                )
+                return self.form_invalid(form)
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        return self.form_invalid(form)
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    success_url = reverse_lazy('catalog:product_list')
-
-
-
+    success_url = reverse_lazy("catalog:product_list")
